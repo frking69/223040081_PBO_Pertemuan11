19407929f3b22307e877c8101db703d73e0fda17
frking69 <121990793+frking69@users.noreply.github.com>
2024-05-21 20:37:30 +0700

    Create KartuElektronik.java

---
+++ KartuElektronik.java
@@ -0,0 +1,25 @@
+public class KartuElektronik implements Kartu {
+    private String kodeBank;
+    private String pin;
+
+    public KartuElektronik(String kodeBank, String pin) {
+        super();
+        this.kodeBank = kodeBank;
+        this.pin = pin;
+    }
+
+    @Override
+    public boolean otentikasi(String pinInput) {
+        if (pin.equals(pinInput))
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public String encode(String pin) {
+        // melakukan enkripsi inputan PIN
+        return null;
+    }
+
+}
